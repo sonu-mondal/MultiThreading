def5471e42a24b72d50fe89af33e0f8148d5937f
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-02-25 00:00:54 +0530

    added more example

--- Java/src/Multithreading_Practice/MyThreadClass.java
+++ Java/src/Multithreading_Practice/MyThreadClass.java
@@ -7,4 +7,11 @@ public class MyThreadClass extends Thread{
 			System.out.println("Child Thread");
 		}
 	}
+	
+	//Overloading the thread class run method
+	public void run(String str) {
+		for(int i=0;i<10;i++) {
+			System.out.println(str);
+		}
+	}
 }
--- Java/src/Multithreading_Practice/ThreadDemo1.java
+++ Java/src/Multithreading_Practice/ThreadDemo1.java
@@ -4,9 +4,11 @@ public class ThreadDemo1 {
 
 	public static void main(String[] args) throws Exception {
 		MyThreadClass t=new MyThreadClass();
-		t.start();
-		t.sleep(5000);
+		//t.start();
+		//t.sleep(5000);
+		//t.run();//when we used run() method in place of start() method then new thread wont be created and run() method will be executed just like a normal method call
 		
+		t.run("Sonu Mondal");//overloading the run() method
 		for(int i=0;i<10;i++) {
 			System.out.println("Main Thread");
 		}
