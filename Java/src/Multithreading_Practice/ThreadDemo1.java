3e9b587c2ec90af285148fad4704370d5e19ca9f
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-03-01 18:55:11 +0530

    setting thread priority

--- Java/src/Multithreading_Practice/ThreadDemo1.java
+++ Java/src/Multithreading_Practice/ThreadDemo1.java
@@ -4,11 +4,13 @@ public class ThreadDemo1 {
 
 	public static void main(String[] args) throws Exception {
 		MyThreadClass t=new MyThreadClass();
-		//t.start();
+		Thread.currentThread().setPriority(10);
+		t.start();
+//		t.setPriority(10);
 		//t.sleep(5000);
 		//t.run();//when we used run() method in place of start() method then new thread wont be created and run() method will be executed just like a normal method call
 		
-		t.run("Sonu Mondal");//overloading the run() method
+		//t.run("Sonu Mondal");//overloading the run() method
 		for(int i=0;i<10;i++) {
 			System.out.println("Main Thread");
 		}
--- Java/src/Thread_Using_Runnable_Interface/ThreadRunnable.java
+++ Java/src/Thread_Using_Runnable_Interface/ThreadRunnable.java
@@ -4,16 +4,22 @@ package Thread_Using_Runnable_Interface;
 public class ThreadRunnable {
 
 	public static void main(String[] args) {
-		System.out.println(Thread.currentThread().getName()+" Thread ");
+		//System.out.println(Thread.currentThread().getName()+" Thread ");
+		//System.out.println("Thread priority is : "+Thread.currentThread().getPriority());
 		MyRunnable r=new MyRunnable();
 		Thread t=new Thread(r);
+		t.setPriority(10);//by default main thread has priority 5 and here we are setting child thread priority as 10 
+		//so child thread will execute first
+		
 		t.start();
+//		Thread.currentThread().setPriority(10);
 		Thread.currentThread().setName("Sonu's thread");
 		
 		for(int i=0;i<10;i++) {
 			System.out.println("Main Thread - Runnable Interface");
 		}
 		System.out.println(Thread.currentThread().getName());
+		System.out.println(Thread.currentThread().getPriority());
 		
 
 	}
