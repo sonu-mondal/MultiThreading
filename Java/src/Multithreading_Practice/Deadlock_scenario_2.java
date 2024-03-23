a4f4b1ca07319316a40e43f5cfd628f512bb3dd2
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-03-23 21:49:03 +0530

    2nd scenario of deadlock

---
+++ Java/src/Multithreading_Practice/Deadlock_scenario_2.java
@@ -0,0 +1,11 @@
+package Multithreading_Practice;
+
+public class Deadlock_scenario_2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread.currentThread().join();
+		System.out.println("Main Thread");
+
+	}
+
+}
